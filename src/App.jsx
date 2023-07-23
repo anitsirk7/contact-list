6eeec8d8cf9e45d961c0538fc3e9ffe354f60002
Kristina Acheampong <kri.acheampong@gmail.com>
2023-07-23 19:12:38 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      
+      <div>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
